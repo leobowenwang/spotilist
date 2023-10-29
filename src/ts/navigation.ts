27a713006f7f1cad9c51dabe5dab0df3d016ef7e
Leo Bowen Wang <leobwang@gmail.com>
2023-10-29 05:01:41 +0100

    add burger menu toggle

--- src/ts/navigation.ts
+++ src/ts/navigation.ts
@@ -6,10 +6,29 @@ async function loadHTMLFile(filePath: string): Promise<string> {
   return await response.text();
 }
 
+function toggleMenu(): void {
+  const menu = document.getElementById("navbarResponsive");
+  const body = document.body;
+  if (menu && body) {
+    if (menu.classList.contains("collapse")) {
+      menu.classList.remove("collapse");
+      body.style.marginTop = "50px";
+    } else {
+      menu.classList.add("collapse");
+      body.style.marginTop = "0";
+    }
+  }
+}
+
 export async function navigation(): Promise<void> {
   const navContainer = document.getElementById("navbar-container");
   if (navContainer) {
     navContainer.innerHTML = await loadHTMLFile("navigation.html");
+
+    const burgerIcon = document.querySelector(".navbar-toggler");
+    if (burgerIcon) {
+      burgerIcon.addEventListener("click", toggleMenu);
+    }
   }
 }
 
